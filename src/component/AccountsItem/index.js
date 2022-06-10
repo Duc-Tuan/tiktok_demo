16de5908b82ea169a081eedf46f20fd7931dbcd9
Tuan <phamductuan26032002@gmail.com>
2022-06-10 17:37:36 +0700

    update

---
+++ src/component/AccountsItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountsItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountsItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/ddcb57a7bcd8bf0fc01c18338b2caf59~c5_300x300.webp?x-expires=1654880400&x-signature=h20mu1akX3Zz8p3Tfl8SCo0s1n0%3D"
+        alt="Hoa"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountsItem;
--- src/component/Layouts/components/Header/index.js
+++ src/component/Layouts/components/Header/index.js
@@ -1,32 +1,89 @@
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircle, faCircleQuestion, faCircleXmark, faEarthAsia, faEllipsisVertical, faKeyboard, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
 
 import classNames from 'classnames/bind';
 import style from './Header.module.scss';
 import images from '~/assets/images';
+import { Wrapper as PopperWrapper } from '~/component/Popper';
+import AccountsItem from '~/component/AccountsItem';
+import Button from '~/component/Button';
+import Menu from '~/component/Popper/Menu';
 
 const cx = classNames.bind(style);
 
+const MENU_ITEMS = [
+  {
+    icon: <FontAwesomeIcon icon={faEarthAsia}/>,
+    title: 'English',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCircleQuestion}/>,
+    title: 'Feedback and help',
+    to: '/feedback'
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard}/>,
+    title: 'Keyboard shortcuts',
+  },
+];
+
 function Header() {
+  const [searchResult, setsearchResult] = useState([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      setsearchResult([]);
+    }, 0);
+  }, []);
+
   return (
     <header className={cx('wapper')}>
       <div className={cx('inner')}>
         <img src={images.logo} alt="tiktok" />
 
-        <div className={cx('search')}>
-            <input placeholder='Search accounts and videos' spellCheck={false} />
+        <Tippy
+          interactive
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx('search-title')}>
+                  Accounts
+                </h4>
+                <AccountsItem/>
+                <AccountsItem/>
+                <AccountsItem/>
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx('search')}>
+            <input placeholder="Search accounts and videos" spellCheck={false} />
             <button>
-                <FontAwesomeIcon className={cx('clear')} icon={ faCircleXmark } />
+              <FontAwesomeIcon className={cx('clear')} icon={faCircleXmark} />
             </button>
-                <FontAwesomeIcon className={cx('laoding')} icon={ faSpinner } />
+            <FontAwesomeIcon className={cx('laoding')} icon={faSpinner} />
             <button className={cx('search-btn')}>
-                <FontAwesomeIcon icon={ faMagnifyingGlass } />
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
-        </div>
+          </div>
+        </Tippy>
 
         <div className={cx('action')}>
+          <Button text>Update</Button>
+          <Button primary>Log in</Button>
 
+          <Menu
+            items={MENU_ITEMS}
+          >
+            <button className={cx('more-icon')}>
+              <FontAwesomeIcon icon={faEllipsisVertical} />
+            </button>
+          </Menu>
         </div>
+
       </div>
     </header>
   );
